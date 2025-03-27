2d449da7defdaaf355d1c695e2865ccdeb21a28c
MichaelVenturi <michaelventuri58@aol.com>
2025-03-27 16:54:19 -0400

    boilerplate

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("test");
+import express from "express";
+import { config } from "dotenv";
+
+// config dotenv
+config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(201).json({ message: "Welcome" });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
