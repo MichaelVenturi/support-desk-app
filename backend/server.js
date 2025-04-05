fe3e958fe99ca7ed982826dfd04fab5a7bc62adb
MichaelVenturi <michaelventuri58@aol.com>
2025-04-05 19:24:43 -0400

    prepare for heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import colors from "colors"; // used elsewhere
 import cors from "cors";
 import { config } from "dotenv";
@@ -20,12 +21,21 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.status(201).json({ message: "Welcome" });
-});
-
+// routes
 app.use("/api/users", userRoutes);
 app.use("/api/tickets", ticketRoutes);
+
+// serve frontend
+if (process.env.NODE_ENV === "production") {
+  // set build folder as static
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => res.sendFile(__dirname, "../", "client", "dist", "index.html"));
+} else {
+  app.get("/", (req, res) => {
+    res.status(201).json({ message: "Welcome to the support Desk API" });
+  });
+}
 
 app.use(errorHandler);
 
